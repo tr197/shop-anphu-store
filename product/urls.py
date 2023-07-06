924fc2084e70f866af1991efed2c4d4b510d6c71
tri266 <huutri26697@gmail.com>
2023-07-06 23:39:46 +0700

    230706 something

--- product/urls.py
+++ product/urls.py
@@ -5,6 +5,9 @@ app_name = 'product'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('dong-san-pham/', views.show_category, name='category'),
+    path('san-pham/', views.show_all_categories, name='category'),
+    path('san-pham/<slug:slug>/<str:category_id>/', views.show_list_categories, name='list-category'),
+    path('san-pham/<slug:slug>/<slug:sub_slug>/<str:sub_category_id>',
+          views.show_list_products, name='list-products'),
 ]
 
--- product/views.py
+++ product/views.py
@@ -1,9 +1,57 @@
+import contextlib
 from django.shortcuts import render
-
+from product.models import Category, SubCategory, Product
 
 def index(request):
     return render(request, 'product/index.html')
 
 
-def show_category(request):
-    return render(request, 'product/page_category.html')
+def show_all_categories(request):
+    all_categories = Category.objects.all()
+
+    context = {
+        'all_categories': all_categories,
+        'list_categories': all_categories,
+    }
+    return render(request, 'product/page_products.html', context)
+
+
+
+def show_list_categories(request, slug, category_id):
+    all_categories = Category.objects.all()
+
+    category = None
+    with contextlib.suppress(Exception):
+        category = Category.objects.get(id=category_id)
+
+    list_category = category.children.all()
+    list_products = category.products.all()
+
+    context = {
+        'all_categories': all_categories,
+        'list_categories': list_category,
+        'list_products': list_products,
+        'category': category
+    }
+    return render(request, 'product/page_products.html', context)
+
+
+def show_list_products(request, slug, sub_slug, sub_category_id):
+    all_categories = Category.objects.all()
+
+    sub_category, list_products = None, None
+    sub_category = None
+    with contextlib.suppress(Exception):
+        sub_category = SubCategory.objects.get(id=sub_category_id)
+        list_products = sub_category.products.all()
+        
+
+    context = {
+        'all_categories': all_categories,
+        'list_products': list_products,
+        'category': sub_category.parent_category,
+        'sub_category': sub_category
+    }
+    return render(request, 'product/page_products.html', context)
+
+# show_list_products
