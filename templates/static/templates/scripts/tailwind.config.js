21673f776d33d6ef41a43d96ca8414e64cb4e274
tri266 <huutri26697@gmail.com>
2023-07-07 17:28:57 +0700

    230707 some colors

---
+++ templates/static/templates/scripts/tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'cdark': '#211E61',
+          'cmedium': '#4F46E5',
+          'clight': '#6366F1',
+        }
+      }
+    }
+  }
